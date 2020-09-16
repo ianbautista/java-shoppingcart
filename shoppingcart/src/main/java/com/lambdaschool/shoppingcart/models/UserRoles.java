e0963fe10302e46c977529271b193f5c92ff7ad5
ianbautista <ianfbautista@gmail.com>
2020-09-16 04:36:05 -0700

    moved the final data.sql file to resources

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/UserRoles.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/UserRoles.java
@@ -24,4 +24,58 @@ public class UserRoles extends Auditable implements Serializable
     @JsonIgnoreProperties(value = "users", allowSetters = true)
     private Roles role;
 
+    public UserRoles()
+    {
+        // default constructor
+    }
+
+    // constructor
+    public UserRoles(User user, Roles role)
+    {
+        this.user = user;
+        this.role = role;
+    }
+
+    // getters and setters
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+
+    public Roles getRole()
+    {
+        return role;
+    }
+
+    public void setRole(Roles role)
+    {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (!(obj instanceof UserRoles))
+        {
+            return false;
+        }
+        UserRoles that = (UserRoles) obj;
+        return ((user == null) ? 0 : user.getUserid()) == ((that.user == null) ? 0 : that.user.getUserid()) &&
+                ((role == null) ? 0 : role.getRoleid()) == ((that.role == null) ? 0 : that.role.getRoleid());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return 69;
+    }
 }
