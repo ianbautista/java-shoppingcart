a20411f62044411e823650456a754b7592dc7006
ianbautista <ianfbautista@gmail.com>
2020-09-16 04:04:56 -0700

    created UserRoles class model join table.

---
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/UserRoles.java
@@ -0,0 +1,28 @@
+package com.lambdaschool.shoppingcart.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.management.relation.Role;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "userroles")
+//@IdClass(UserRolesId.class)
+public class UserRoles extends Auditable implements Serializable
+{
+    // foreign key. relationship to User
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "userid")
+    @JsonIgnoreProperties(value = "roles", allowSetters = true)
+    private User user;
+
+    // foreign key. relationship to Role
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "roleid")
+    @JsonIgnoreProperties(value = "users", allowSetters = true)
+    private Role role;
+
+}
